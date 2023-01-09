adac29f82a7bffb04536774dd13b30ca3bc4e116
AlphaShk <shkoladenys@gmail.com>
2023-01-09 22:34:51 +0100

    Initial Commit

---
+++ chatbot/chatbotApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatbotApp.swift
+//  chatbot
+//
+//  Created by Denys Shkola on 09.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct chatbotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
